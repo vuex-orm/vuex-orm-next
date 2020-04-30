23584dad7554b992cc01c956277204991478b96c
Kia Ishii <kia.king.08@gmail.com>
2020-05-01 00:31:19 +0900

    feat: throw error when accessing `model.$store` without store being injected

---
+++ test/unit/model/Model.spec.ts
@@ -0,0 +1,11 @@
+import { Model } from '@/model/Model'
+
+describe('unit/model/Model', () => {
+  class User extends Model {
+    static entity = 'users'
+  }
+
+  it('throws when accessing the store but it is not injected', () => {
+    expect(() => new User().$store).toThrow()
+  })
+})
