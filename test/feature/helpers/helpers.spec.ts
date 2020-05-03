4587db7599210d25f077cd2818b038c4982ed892
Kia King Ishii <kia.king.08@gmail.com>
2020-05-03 19:32:07 +0900

    feat: add `mapRepos` helper (#5)

---
+++ test/feature/helpers/helpers.spec.ts
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import { createStore } from 'test/Helpers'
+import { Model, Attr, Str, Repository, mapRepos } from '@/index'
+
+describe('feature/helpers/helpers', () => {
+  class User extends Model {
+    static entity = 'users'
+
+    @Attr() id!: any
+    @Str('') name!: string
+  }
+
+  class UserRepository extends Repository<User> {
+    use = User
+  }
+
+  it('maps repositories to vue component from models', async () => {
+    const store = createStore([User])
+
+    const vm = new Vue({
+      store,
+      computed: mapRepos({
+        userRepo: User
+      })
+    })
+
+    expect(vm.userRepo).toBeInstanceOf(Repository)
+    expect(vm.userRepo.getModel()).toBeInstanceOf(User)
+  })
+
+  it('maps repositories to vue component from repositories', async () => {
+    const store = createStore([User])
+
+    const vm = new Vue({
+      store,
+      computed: mapRepos({
+        userRepo: UserRepository
+      })
+    })
+
+    expect(vm.userRepo).toBeInstanceOf(Repository)
+    expect(vm.userRepo.getModel()).toBeInstanceOf(User)
+  })
+})
