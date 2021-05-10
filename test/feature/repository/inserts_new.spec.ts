41d4c8c52091b1c31100f95621a89529dfbcd7a8
Cue <1493221+cuebit@users.noreply.github.com>
2021-05-10 14:23:26 +0100

    feat: add the `new` method to create and persist entry with default values (#57) (#68)
    close #57

---
+++ test/feature/repository/inserts_new.spec.ts
@@ -0,0 +1,40 @@
+import { createStore, assertState, mockUid } from 'test/Helpers'
+import { Model, Str, Num, Bool, Uid, Attr } from '@/index'
+
+describe('feature/repository/inserts_new', () => {
+  it('inserts with a models default values', async () => {
+    class User extends Model {
+      static entity = 'users'
+
+      @Uid() id!: string
+      @Str('John Doe') name!: string
+      @Num(21) age!: number
+      @Bool(true) active!: boolean
+    }
+
+    mockUid(['uid1'])
+
+    const store = createStore()
+
+    await store.$repo(User).new()
+
+    assertState(store, {
+      users: {
+        uid1: { id: 'uid1', name: 'John Doe', age: 21, active: true }
+      }
+    })
+  })
+
+  it('throws if a primary key is not capable of being generated', async () => {
+    class User extends Model {
+      static entity = 'users'
+
+      @Attr() id!: any
+      @Str('John Doe') name!: string
+    }
+
+    const store = createStore()
+
+    await expect(() => store.$repo(User).new()).rejects.toThrow()
+  })
+})
