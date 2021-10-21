ab1a5a87c99597fdfec6b1c7ee7fec48913425a4
ryandialpad <ryan.lafferty@dialpad.com>
2021-10-21 14:17:30 -0700

    Init morph_one_retrieve feature test

---
+++ test/feature/relations/morph_one_retrieve.spec.ts
@@ -0,0 +1,129 @@
+import { createStore, fillState, assertModel } from 'test/Helpers'
+import { Model, Attr, Str, MorphOne } from '@/index'
+
+/*
+  Potential Improvements
+   - DRY data / classes
+   - DRY test cases
+*/
+describe('feature/relations/morph_one_retrieve', () => {
+  class Image extends Model {
+    static entity = 'images'
+
+    @Attr() id!: number
+    @Str('') url!: string
+    @Attr() imageable_id!: number
+    @Attr() imageable_type!: string
+  }
+
+  class User extends Model {
+    static entity = 'users'
+
+    @Attr() id!: number
+    @Str('') name!: string
+
+    @MorphOne(() => Image, 'imageable_id', 'imageable_type')
+    image!: Image | null
+  }
+
+  class Post extends Model {
+    static entity = 'posts'
+
+    @Attr() id!: number
+    @Str('') title!: string
+    @Attr() userId!: number
+    @MorphOne(() => Image, 'imageable_id', 'imageable_type')
+    image!: Image | null
+  }
+
+  describe('when there are images', () => {
+    const store = createStore()
+
+    fillState(store, {
+      users: {
+        1: { id: 1, name: 'John Doe' }
+      },
+      posts: {
+        1: { id: 1, title: 'Hello, world!' },
+        2: { id: 2, title: 'Hello, world! Again!' }
+      },
+      images: {
+        1: {
+          id: 1,
+          url: '/profile.jpg',
+          imageable_id: 1,
+          imageable_type: 'users'
+        },
+        2: {
+          id: 2,
+          url: '/post.jpg',
+          imageable_id: 1,
+          imageable_type: 'posts'
+        },
+        3: {
+          id: 3,
+          url: '/post2.jpg',
+          imageable_id: 2,
+          imageable_type: 'posts'
+        }
+      }
+    })
+
+    it('can eager load morph one relation for user', () => {
+      const user = store.$repo(User).with('image').first()!
+  
+      expect(user).toBeInstanceOf(User)
+      expect(user.image).toBeInstanceOf(Image)
+      assertModel(user, {
+        id: 1,
+        name: 'John Doe',
+        image: {
+          id: 1,
+          url: '/profile.jpg',
+          imageable_id: 1,
+          imageable_type: 'users'
+        }
+      })
+    })
+
+    it('can eager load morph one relation for post', () => {
+      const post = store.$repo(Post).with('image').first()!
+  
+      expect(post).toBeInstanceOf(Post)
+      expect(post.image).toBeInstanceOf(Image)
+      assertModel(post, {
+        id: 1,
+        title: 'Hello, world!',
+        image: {
+          id: 2,
+          url: '/post.jpg',
+          imageable_id: 1,
+          imageable_type: 'posts'
+        }
+      })
+    })
+  })
+
+  describe('when there are no images', () => {
+    const store = createStore()
+
+    fillState(store, {
+      users: {
+        1: { id: 1, name: 'John Doe' }
+      },
+      posts: {},
+      images: {}
+    })
+
+    it('can eager load missing relation as `null`', () => {
+      const user = store.$repo(User).with('image').first()!
+  
+      expect(user).toBeInstanceOf(User)
+      assertModel(user, {
+        id: 1,
+        name: 'John Doe',
+        image: null
+      })
+    })
+  })
+})
